a4aec3de1e844eb2161db8df176086f9d96b3041
Alex <alee.exe1403@gmail.com>
2026-01-14 18:16:25 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PlayerProvider } from "@/contexts/PlayerContext";
-import { BottomNavigation } from "@/components/BottomNavigation";
-import { MiniPlayer } from "@/components/MiniPlayer";
-import { FullPlayer } from "@/components/FullPlayer";
-import Index from "./pages/Index";
-import { Search } from "./pages/Search";
-import { Library } from "./pages/Library";
-import { Settings } from "./pages/Settings";
-import NotFound from "./pages/NotFound";
+import { PlayerProvider } from '@/contexts/PlayerContext';
+import { Player } from '@/components/Player';
+import { FullPlayer } from '@/components/FullPlayer';
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <PlayerProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <div className="min-h-screen bg-background">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/library" element={<Library />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <MiniPlayer />
-            <FullPlayer />
-            <BottomNavigation />
-          </div>
-        </BrowserRouter>
-      </PlayerProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <PlayerProvider>
+      <div className="max-w-md mx-auto mt-10">
+        <Player />
+        <FullPlayer />
+      </div>
+    </PlayerProvider>
+  );
+}
